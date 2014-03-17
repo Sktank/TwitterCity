833978e6777e6ab40f72b0a6ab7de7f3ff58bc02
Spencer <stank@dimagi.com>
2014-03-17 12:56:23 -0400

    basic node server

--- server.js
+++ server.js
@@ -8,3 +8,24 @@
 
 
 // this is where the node server is going to go
+
+var express = require('express'),
+    jade = require('jade'),
+    http = require('http'),
+    app = express(),
+    server = http.createServer(app),
+    io = require('socket.io').listen(server);
+
+server.listen(4000);
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.set("view options", { layout: false });
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', function (req, res) {
+    res.render(
+        'home.jade'
+    );
+});
