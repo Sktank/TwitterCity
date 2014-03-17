ab42a9388819afcf2409b41b4760238d10c970e5
Spencer <stank@dimagi.com>
2014-03-17 12:38:39 -0400

    init

---
+++ server.js
@@ -0,0 +1,10 @@
+/**
+ * Created with PyCharm.
+ * User: spencertank
+ * Date: 3/17/14
+ * Time: 12:36 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+// this is where the node server is going to go
