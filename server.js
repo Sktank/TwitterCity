a00fa38583fbfb6a96e03a1ca551fca4e59cdd88
Spencer <stank@dimagi.com>
2014-03-17 13:05:27 -0400

    integration with twitter stream

--- server.js
+++ server.js
@@ -7,25 +7,45 @@
  */
 
 
-// this is where the node server is going to go
+// Node Imports
 
 var express = require('express'),
     jade = require('jade'),
     http = require('http'),
+    twitter = require('ntwitter'),
     app = express(),
     server = http.createServer(app),
     io = require('socket.io').listen(server);
 
-server.listen(4000);
 
+// Server Config
+
+server.listen(4000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.set("view options", { layout: false });
 app.use(express.static(__dirname + '/public'));
 
+var twit = new twitter({
+    consumer_key: 'JLKj3orcc4N4l9H9RVfHw',
+    consumer_secret: 'bhhHXyDiQkQymlcR4jTCAAFYq6YirK0mrKJ3Loxc0',
+    access_token_key: '2394477463-fQf4XuEIYNO6W70BD7xtiXcWzcgd1Or9cdab8cU',
+    access_token_secret: '5Il4HWeqCab1Z1sPF1W7DuIlPOM6cSp4H6aQQ1C0jiFnC'
+});
+
+
+// Routes
 
 app.get('/', function (req, res) {
     res.render(
         'home.jade'
     );
 });
+
+twit.stream('statuses/sample', function(stream) {
+    stream.on('data', function (data) {
+        console.log(data);
+    });
+});
+// Twitter Stream
+
